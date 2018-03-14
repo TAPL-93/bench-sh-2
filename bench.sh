8d3e8d894b4eacd5111c5895baa1e3b4ba655012
TAPL-93 <tapl933@gmail.com>
2018-03-14 22:14:00 +0300

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -148,7 +148,7 @@ iotest () {
 	echo "I/O (1st run)	: $io" | tee -a $HOME/bench.log
 	echo "I/O (2nd run)	: $io2" | tee -a $HOME/bench.log
 	echo "I/O (3rd run)	: $io3" | tee -a $HOME/bench.log
-	echo "Average I/O	: $ioavg MB/s" | tee -a $HOME/bench.log
+	echo "Average I/O	: $ioavg GB/s" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 }
 gbench () {
